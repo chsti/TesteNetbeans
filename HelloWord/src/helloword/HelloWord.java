4996550a3a785f11a48a15e0f5e4fe1724f6e792
chsti <chsti@chsti-NE56R>
2017-11-15 02:21:58 -0200

    Teste de Integração Netbeans

--- HelloWord/src/helloword/HelloWord.java
+++ HelloWord/src/helloword/HelloWord.java
@@ -17,6 +17,8 @@ public class HelloWord {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello World");
+        //Atualizando
+        System.out.println("Teste Atualização");
     }
     
 }
